f3624769a5646f768fbd8e88808565038dd39e9b
Nazrul Islam <mhdnazrul511@email.com>
2026-06-14 14:08:06 +0600

    refactor: optimize ThemeProvider

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -1,18 +1,18 @@
-"use client";
+'use client';
 
-import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { ReactNode } from "react";
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import { ReactNode } from 'react';
 
 export function ThemeProvider({ children }: { children: ReactNode }) {
-  return (
-    <NextThemesProvider
-      attribute="class"
-      defaultTheme="dark"
-      enableSystem={false}
-      disableTransitionOnChange={false}
-      storageKey="portfolio-theme"
-    >
-      {children}
-    </NextThemesProvider>
-  );
+    return (
+        <NextThemesProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem={false}
+            disableTransitionOnChange={false}
+            storageKey="portfolio-theme"
+        >
+            {children}
+        </NextThemesProvider>
+    );
 }
